3cfe08a4a425635994866c1acb58380cd132ce8c
Patrick Kee <patrick.kee0@gmail.com>
2015-09-08 22:39:16 -0400

    added unit tests for body metrics

--- src/main/java/metric/Body.java
+++ src/main/java/metric/Body.java
@@ -7,50 +7,61 @@ import org.json.JSONObject;
 
 import com.google.common.base.Preconditions;
 
+/**
+ * Represents a measurement about weight and skinfolds at a point in time
+ */
 public class Body implements BodyType {
 
-	private Date loDate;
-	private float loChestFold;
-	private float loStomachFold;
-	private float loThighFold;
-	private float loWeight;
+	private Date _date;
+	private float _chestFold;
+	private float _stomachFold;
+	private float _thighFold;
+	private float _weight;
 	
 	private Body(Date date, float chestFold, float stomachFold, float thighFold, float weight) {
 		Preconditions.checkNotNull(date);
-		loDate = date;
-		loChestFold = chestFold;
-		loStomachFold = stomachFold;
-		loThighFold = thighFold;
-		loWeight = weight;
+		_date = date;
+		
+		if (chestFold 	 <=0 || 
+			stomachFold <=0 ||
+			thighFold 	 <=0 || 
+			weight 	 <=0) {
+			throw new IllegalArgumentException();
+		} else {
+			_chestFold = chestFold;
+			_stomachFold = stomachFold;
+			_thighFold = thighFold;
+			_weight = weight;	
+		}
 	}
 
-	public static Body getNew(Date date, float chestFold, float stomachFold, float thighFold, float weight) {
+	public static Body getNew(Date date, float chestFold, float stomachFold, float thighFold, float weight) throws NullPointerException, IllegalArgumentException {
 		return new Body(date, chestFold, stomachFold, thighFold, weight);
 	}
 
 	@Override
 	public Date getDate() {
-		return loDate;
+		return _date;
 	}
 
 	@Override
 	public float getChestFold() {
-		return loChestFold;
+		return _chestFold;
 	}
 
 	@Override
 	public float getStomachFold() {
-		return loStomachFold;
+		return _stomachFold;
 	}
 
 	@Override
 	public float getThighFold() {
-		return loThighFold;
+		return _thighFold;
 	}
 
 	@Override
 	public float getWeight() {
-		return loWeight;
+		return _weight;
 	}
 
 	public String toString() {
--- src/test/java/body/testBody.java
+++ src/test/java/body/testBody.java
@@ -6,6 +6,7 @@ import java.util.Date;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import metric.Body;
@@ -30,25 +31,40 @@ public class testBody {
 	}
 	
 	@Test
-	public void testAddMetric() throws Exception {
-		Date mDate = new Date();
-		float chestSkinfold = 5;
-		float stomachSkinfold = 19;
-		float thighSkinfold = 8;
-		float weight = (float) 187.5;
-		
-		Body metric = Body.getNew(mDate, chestSkinfold, stomachSkinfold, thighSkinfold, weight);	
-		System.out.println(metric.toString());
+	public void testAddMetric() {
+		Body metric = Body.getNew(new Date(), (float) 5, (float) 1, (float) 4, (float) 180.63);
 		assertEquals(200, PersistenceController.addMetric(metric));
 	}
 	
-	@Test
-	public void testGetMetric() throws Exception {
-		assertEquals(1,1);
+	
+	@Test(expected=NullPointerException.class)
+	public void testAddMetricNullDate() {
+		Body metric = Body.getNew(null, (float) 5, (float) 1, (float) 4, (float) 180.63);	
+	}
+
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddMetricIllegalChestFold() {
+		Body metric = Body.getNew(new Date(), (float) 0, (float) 5, (float) 6, (float) -1);		
 	}
 	
-	@After
-	public void tearDown() throws Exception {
-		
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddMetricIllegalStomachFold() {
+		Date someDate = new Date();
+		Body metric = Body.getNew(someDate, (float) 2, (float) 0, (float) 6, (float) 6);		
 	}
+	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddMetricIllegalThighFold() {
+		Body metric = Body.getNew(new Date(), (float) 2, (float) 5, (float) 0, (float) -1);	
+	}
+	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testAddMetricIllegalWeight() {
+		Body metric = Body.getNew(new Date(), (float) 2, (float) 5, (float) 6, (float) 0);	
+	}
+	
 }
